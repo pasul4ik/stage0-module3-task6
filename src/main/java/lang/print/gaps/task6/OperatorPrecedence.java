663b866da0b24cbfbd5d689cfb2313bf268103ba
PavelDenisov <pavel.den111@gmail.com>
2023-02-14 10:03:49 +0200

    done

--- src/main/java/lang/print/gaps/task6/OperatorPrecedence.java
+++ src/main/java/lang/print/gaps/task6/OperatorPrecedence.java
@@ -4,7 +4,7 @@ public class OperatorPrecedence {
     public static void main(String[] args) {
         int first = 1;
         int second = 2;
-        int eight = 3 + first * second;
+        int eight = 3 + (first * second);
         System.out.println(eight);//eight is expected to be 8
         boolean firstBool = false;
         boolean secondBool = false;
@@ -12,7 +12,17 @@ public class OperatorPrecedence {
         boolean fourthBool = 4 <= 101;
         int varSix = 17;
 
-        System.out.println(firstBool && secondBool || thirdBool && fourthBool || (varSix = 5) < 0);
+        System.out.println((firstBool && secondBool) || (thirdBool && fourthBool) || (varSix = 5) < 0);
         System.out.println(varSix);
     }
 }
+//4) Operator precedence.
+//        Introduce changes (add brackets) to code snippets below to get expected results
+//        (for System.out.println(firstBool && secondBool || thirdBool && fourthBool || (varSix = 5) < 0);
+//        only adding brackets is allowed):
+//Expected console output:
+//
+//        8
+//
+//        false
+//5
